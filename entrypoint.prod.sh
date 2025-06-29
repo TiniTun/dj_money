6625d9ea9d47cb3e8352fc4f88a65e63ec943cf7
Egor Rydannykh <caesargu@gmail.com>
2025-06-30 00:31:48 +1000

    fix privileges for file tmp in conteiner

---
+++ entrypoint.prod.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This script is executed as root.
+
+# Set ownership of the shared volume so that the 'app' user can write to it.
+# This is necessary because Docker mounts named volumes as root.
+chown -R app:app /home/app/web/tmp
+
+# Drop privileges and execute the main command (passed as arguments to this script)
+# as the non-root 'app' user.
+exec su-exec app "$@"
